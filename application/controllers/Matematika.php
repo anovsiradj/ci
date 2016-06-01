03157a9154f62be6b291b19c029f2e57bcbd268f
anovsiradj <anov.siradj22@gmail.com>
2016-06-02 00:48:06 +0700

    mtk: menambahkan link kembali
    serta merapikan dan beberapa komentar

--- application/controllers/Matematika.php
+++ application/controllers/Matematika.php
@@ -1,71 +1,70 @@
 <?php // 201605272222, 201605281252
 class Matematika extends CI_Controller
 {
-	function __construct() {
-
+	function __construct()
+	{
 		// method ini optional.
 
 		// jika ct ada method ini,
 		// harus panggil manual parent.
-		// $this yg awalnya cuma {} biasa, menjadi {} ci.
+		// $this yg awalnya cuma {} biasa, menjadi {} ci ct.
 		// dimana disediakan akses untuk menggunakan fitur2nya.
 		parent::__construct();
 
-		// ::load otomatis ada jika parent dari method ini (construct) sudah dipanggil
+		// property $this->load otomatis ada jika parent::__construct sudah dipanggil.
+		// atau dalam ct ini tidak ada method __construct
 		$this->load->view('mtk/index');
-
-
 	}
+
 	// harus ada.
 	// fungsi yg dijalankan saat class dipanggil tanpa method.
-	public function index() {
-
+	public function index()
+	{
 		$this->load->view('mtk/operator');
-
 	}
 
-	public function tambah($a = 0,$b = 0) {
+	public function tambah($a = 0,$b = 0)
+	{
 		$res = $a+$b;
 		$this->load->view('mtk/tambah',[
 			'a' => $a,
 			'b' => $b,
 			'res' => $res
 		]);
+		$this->load->view('mtk/_kembali');
 	}
 
-	public function kurang($a = 0,$b = 0) {
+	public function kurang($a = 0,$b = 0)
+	{
 		$res = $a-$b;
 		$this->load->view('mtk/kurang',[
 			'a' => $a,
 			'b' => $b,
 			'res' => $res
 		]);
+		$this->load->view('mtk/_kembali');
 	}
 
-	public function bagi($a = 0,$b = 0) {
+	public function bagi($a = 0,$b = 0)
+	{
 		$res = @($a/$b)+0;
 		$this->load->view('mtk/bagi',[
 			'a' => $a,
 			'b' => $b,
 			'res' => $res
 		]);
+		$this->load->view('mtk/_kembali');
 	}
 
-	public function kali($a = 0,$b = 0) {
+	public function kali($a = 0,$b = 0)
+	{
 		$res = $a*$b;
 		$this->load->view('mtk/kali',[
 			'a' => $a,
 			'b' => $b,
 			'res' => $res
 		]);
-	}
-
-	public function __destruct() {
-		// print(__FILE__);
-		// $this->load->helper('url');
-		// untuk bisa menggunakannya fungsi [base_url], kita harus memanggil helpernya dulu (diatas).
-		// echo "<br/><br/>";
-		// echo "<a href='".base_url(strtolower(__CLASS__))."'>Index</a>";
+		$this->load->view('mtk/_kembali');
 	}
 
 }
